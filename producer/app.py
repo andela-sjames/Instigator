bd7e93c9fe0e9db041ad5a6d5139800367c83388
samuelvarejames <samuelvarejames@gmail.com>
2020-01-01 13:02:38 +0100

    Fix the out of request context issue

--- producer/app.py
+++ producer/app.py
@@ -1,37 +1,51 @@
+import sys
 import threading
+import atexit
 
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-clock_timer = None
+
+WAIT_TIME = 2 #Seconds
+
+globalThread = threading.Thread()
 
 def get_generator():
     import random
+    global globalThread
+    # run outside the request context
+    globalThread = threading.Timer(WAIT_TIME, get_generator, ())
+    globalThread.start()
     resp  = {
         "random_value": random.random() * 5
     }
-    print(resp)
+    print(resp, file=sys.stdout)
 
-@app.route('/', methods=['GET', ])
+@app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 
 @app.route('/start_producer', methods=['GET', ])
 def produce():
-    global clock_timer
-    timer = threading.Timer(20, get_generator)
-    timer.start()
-    clock_timer = timer
-    return 'Producer started'
+    global globalThread
+    if request.method == 'GET':
+        # calls itself the first time and exits the request context
+        globalThread = threading.Timer(WAIT_TIME, get_generator, ())
+        globalThread.start()
+        return 'Producer started'
 
 
 @app.route('/stop_producer', methods=['GET', ])
 def stop():
-    global clock_timer
-    clock_timer.cancel()
-    return 'Producer stoped'
+    global globalThread
+    if request.method == 'GET':
+        globalThread.cancel()
+        return 'Producer stoped'
+    
+    
+# atexit.register(stop)
 
 
 if __name__ == '__main__':
