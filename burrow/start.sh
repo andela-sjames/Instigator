83dc5ae4a85df03e25e8f1ff700fb38d317aebec
samuelvarejames <samuelvarejames@gmail.com>
2020-01-09 23:50:18 +0100

    Use environments for burrows

--- burrow/start.sh
+++ burrow/start.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+
+CONFIG_FILE=/etc/burrow/burrow.toml
+
+sed -i "s~{ZOOKEEPER_SERVERS}~${ZOOKEEPER_SERVERS}~" $CONFIG_FILE
+sed -i "s~{ZOOKEEPER_PATH}~${ZOOKEEPER_PATH}~" $CONFIG_FILE
+sed -i "s~{KAFKA_BROKERS}~${KAFKA_BROKERS}~" $CONFIG_FILE
+
+echo "start"
+cat $CONFIG_FILE
+
 echo "=========start burrow==========="
-exec $GOPATH/bin/Burrow --config-dir /etc/burrow "${@}"
+exec /etc/burrow/burrow --config-dir /etc/burrow "${@}"
 echo "=========starting burrow========"
